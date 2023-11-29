99fd89d3c0f9ca2e96f3d821bdd26a98afad401f
jaklecrack <musse.marina@orange.fr>
2023-11-30 00:53:36 +0100

    working on analysis of unrecognized words using textblob

--- semantic_analysis/sentiment_analysis/stutter_detection.py
+++ semantic_analysis/sentiment_analysis/stutter_detection.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import re
 import csv
+from textblob import TextBlob
 
 # import of the data as csv files
 df_control = pd.read_csv('../control_data_clean.csv')
@@ -37,3 +38,26 @@ percentage_control = nb_word_repetition(df_control)/df_control.shape[0] * 100
 percentage_aphasic = nb_word_repetition(df_aphasic)/df_aphasic.shape[0] * 100
 print('control:' , percentage_control, '%')
 print('aphasic', percentage_aphasic, '%')
+
+
+
+
+
+def nb_syllable_repetition(data):
+    blob = TextBlob(data)
+    blob_words_full_count = 0
+    blob_words_full_list = []
+    unknown_words = []
+    for line in data['text']:
+        str_line = str(line)
+        blob = TextBlob(str_line)
+        blob_words_full_list += blob.words
+        blob_words_full_count += blob.word_counts
+
+    for word in blob_words_full_list:
+        if word not in blob_words_full_count:
+            unknown_words.append(word)
+    return(unknown_words)
+
+print(nb_syllable_repetition(df_control))
+print(nb_syllable_repetition(df_aphasic))
