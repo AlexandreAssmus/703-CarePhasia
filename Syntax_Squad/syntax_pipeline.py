f43dd05b6aab0388858089e2dd0e129771c06765
Belen Saavedra <belen.saavedra.39@gmail.com>
2023-11-30 12:56:35 +0100

    Control data processed with syntax functions

--- Syntax_Squad/syntax_pipeline.py
+++ Syntax_Squad/syntax_pipeline.py
@@ -11,25 +11,53 @@ Patient data syntax processing
 '''
 
 #Process inicial dataframe
-df_raw = pd.read_csv(r'C:\Users\belen\Desktop\Université de Lorraine\703\Aphasia\Data\Cleaned_files\patient_data_clean.csv')
+#df_raw = pd.read_csv(r'C:\Users\belen\Desktop\Université de Lorraine\703\Aphasia\Data\Cleaned_files\patient_data_clean.csv')
 
-processed_df = split_into_clauses(df_raw, 'text')
+#processed_df = split_into_clauses(df_raw, 'text')
 
 #Clause_length metric
-processed_df['Word_Counts'] = processed_df['Split_Clauses'].apply(count_words_in_clause_list_and_return_list)
+#processed_df['Word_Counts'] = processed_df['Split_Clauses'].apply(count_words_in_clause_list_and_return_list)
 
 #Depth_tree metric
-processed_df['Max_Syntax_Tree_Depth'] = processed_df['Split_Clauses'].apply(calculate_max_tree_depth)
+#processed_df['Max_Syntax_Tree_Depth'] = processed_df['Split_Clauses'].apply(calculate_max_tree_depth)
 
 #Word_order metric
-processed_df['Word_Order'] = processed_df['Split_Clauses'].apply(get_word_order_for_clauses)
+#processed_df['Word_Order'] = processed_df['Split_Clauses'].apply(get_word_order_for_clauses)
 
 #Verb_phrases metric
-processed_df['Verb_Phrases'] = processed_df['Split_Clauses'].apply(get_verb_phrase_counts_for_clauses)
+#processed_df['Verb_Phrases'] = processed_df['Split_Clauses'].apply(get_verb_phrase_counts_for_clauses)
+
+#print(processed_df.head())
+
+#Save data
+#path= r'C:\Users\belen\Desktop\Université de Lorraine\703\Aphasia\Data\Processed_data\syntax_data_processed.csv'
+
+#processed_df.to_csv(path, index=False)
+
+'''
+Processed data for controls 
+'''
+
+#Process inicial dataframe
+df_raw_control = pd.read_csv(r'C:\Users\belen\Desktop\Université de Lorraine\703\Aphasia\Data\Cleaned_files\control_data_clean.csv')
+
+processed_df_control = split_into_clauses(df_raw_control, 'text')
+
+#Clause_length metric
+processed_df_control['Word_Counts'] = processed_df_control['Split_Clauses'].apply(count_words_in_clause_list_and_return_list)
+
+#Depth_tree metric
+processed_df_control['Max_Syntax_Tree_Depth'] = processed_df_control['Split_Clauses'].apply(calculate_max_tree_depth)
+
+#Word_order metric
+processed_df_control['Word_Order'] = processed_df_control['Split_Clauses'].apply(get_word_order_for_clauses)
+
+#Verb_phrases metric
+processed_df_control['Verb_Phrases'] = processed_df_control['Split_Clauses'].apply(get_verb_phrase_counts_for_clauses)
 
 print(processed_df.head())
 
 #Save data
-path= r'C:\Users\belen\Desktop\Université de Lorraine\703\Aphasia\Data\Processed_data\syntax_data_processed.csv'
+path= r'C:\Users\belen\Desktop\Université de Lorraine\703\Aphasia\Data\Processed_data\syntax_data_processed_control.csv'
 
-processed_df.to_csv(path, index=False)
+processed_df_control.to_csv(path, index=False)
